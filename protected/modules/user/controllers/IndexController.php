844e3a599f023ef431a2fe4d4d9870787be68bb4
balazs <vargabalazslaszlo@gmail.com>
2013-09-02 12:44:20 -0700

    user module

---
+++ protected/modules/user/controllers/IndexController.php
@@ -0,0 +1,38 @@
+<?php
+
+class IndexController extends Controller
+{
+	public function actionIndex()
+	{
+		$response = array('success' => true, 'data' => array('egy' => 1,'ketto' => 2));
+		
+		$this->renderText(json_encode($response));
+	}
+
+	// Uncomment the following methods and override them if needed
+	/*
+	public function filters()
+	{
+		// return the filter configuration for this controller, e.g.:
+		return array(
+			'inlineFilterName',
+			array(
+				'class'=>'path.to.FilterClass',
+				'propertyName'=>'propertyValue',
+			),
+		);
+	}
+
+	public function actions()
+	{
+		// return external action classes, e.g.:
+		return array(
+			'action1'=>'path.to.ActionClass',
+			'action2'=>array(
+				'class'=>'path.to.AnotherActionClass',
+				'propertyName'=>'propertyValue',
+			),
+		);
+	}
+	*/
+}
